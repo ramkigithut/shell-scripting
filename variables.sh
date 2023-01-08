3dc3bce53c8c4c65e62dded48d1e71a2449185ec
ramkigithut <reddithi.krishna@gmail.com>
2023-01-08 06:50:08 +0530

    Merge remote-tracking branch 'origin/main'

--- variables.sh
+++ variables.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
+#assigning a name to a set of data is termed as a variable
 
-employee=rk
-batch=25
-
-echo employee1 - $employee
-echo batch1 - $batch
-
-echo employee2 - $employee
-echo batch2 - $batch
-date=$(date +%F)
-sum=$((2+3))
-location=$(pwd)
-echo today date is $date
-echo sum is $sum
-place is $location
-
-#end of practice
