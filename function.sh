743c266c74c5065c232832802a2e5442e60a83c6
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 05:00:35 +0530

    example function

---
+++ function.sh
@@ -0,0 +1,11 @@
+#Functions are like variables
+#Variable - when we assign a name to a set of data then it is a variable
+#Function - when we assign a name to a set of commands then it is a function
+
+#Function Declaration
+example() {
+  echo sample statement
+}
+
+##Calling a function is simple , just write name of the function
+example
