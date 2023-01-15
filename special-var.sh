831aff10397b4f89e276ab236477621afeef83e6
ramkigithut <reddithi.krishna@gmail.com>
2023-01-15 17:38:20 +0530

    adding print function

--- special-var.sh
+++ special-var.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+example_1()
+ echo "Following values from function"
+ echo "0 - $0"
+ echo "1 - $1"
+ echo "2 - $2"
+ echo "3 - $3"
+ echo "* - $*"
+ echo "@ - $@"
+ echo "# - $#"
+
+}
+
+example_1 success March12
+
+
+echo "Following values from function"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
